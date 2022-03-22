3c49bd6c5534d19a992e00b0db9b5e24cc504e24
morikentiger <kenrur339@gmail.com>
2022-03-23 04:31:13 +0900

    参考　https://www.youtube.com/watch?v=FasdqcqAiw8

--- X-DAY/ContentView.swift
+++ X-DAY/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var timerMode: TimerMode = .initial
+    
+    @State var selectedPickerIndex = 0
+    
+    let availableMinutes = Array(1...59)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            VStack{
+                Text("60")
+                    .font(.system(size: 80))
+                    .padding(.top, 80)
+                Image(systemName: timerMode == .running ? "pause.circle.fill" :"play.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.red)
+                if timerMode == .paused {
+                    Image(systemName: "gobackward")
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                        .padding(.top, 40)
+                }
+                if timerMode == .initial {
+                    Picker(selection: $selectedPickerIndex, label:Text("")){
+                        ForEach(0 ..< availableMinutes.count) {_ in 
+                            Text("\(self.availableMinutes[0]) min")
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Timer")
+        }
     }
 }
 
---
+++ X-DAY/Helper.swift
@@ -0,0 +1,14 @@
+//
+//  Helper.swift
+//  X-DAY
+//
+//  Created by 森田健太 on 2022/03/23.
+//
+
+import Foundation
+
+enum TimerMode {
+    case running
+    case paused
+    case initial
+}
