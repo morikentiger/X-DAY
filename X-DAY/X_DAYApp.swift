b9398015593c38e983b58b8bd12613b81de340f8
morikentiger <kenrur339@gmail.com>
2022-03-23 03:44:10 +0900

    Initial Commit

---
+++ X-DAY/X_DAYApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_DAYApp.swift
+//  X-DAY
+//
+//  Created by 森田健太 on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct X_DAYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
