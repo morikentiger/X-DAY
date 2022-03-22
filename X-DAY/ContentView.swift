3f17740f4722eb9d84d60cda1d474c78827ee28d
morikentiger <kenrur339@gmail.com>
2022-03-23 08:19:37 +0900

    参考　https://catch-questions.com/posts/activity21.html

--- X-DAY/ContentView.swift
+++ X-DAY/ContentView.swift
@@ -8,41 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var timerMode: TimerMode = .initial
-    
-    @State var selectedPickerIndex = 0
-    
-    let availableMinutes = Array(1...59)
+    var toDate = Calendar.current.date(byAdding: .day, value: 993, to: Date())!
     
     var body: some View {
-        
-        NavigationView{
-            VStack{
-                Text("60")
-                    .font(.system(size: 80))
-                    .padding(.top, 80)
-                Image(systemName: timerMode == .running ? "pause.circle.fill" :"play.circle.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.red)
-                if timerMode == .paused {
-                    Image(systemName: "gobackward")
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 50, height: 50)
-                        .padding(.top, 40)
-                }
-                if timerMode == .initial {
-                    Picker(selection: $selectedPickerIndex, label:Text("")){
-                        ForEach(0 ..< availableMinutes.count) {_ in 
-                            Text("\(self.availableMinutes[0]) min")
-                        }
-                    }
-                }
-            }
-            .navigationBarTitle("Timer")
-        }
+        TimerView(setDate: toDate)
     }
 }
 
@@ -50,4 +19,33 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct TimerView : View {
+    
+    @State var nowDate: Date = Date()
+    let setDate: Date
+    var timer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+            self.nowDate = Date()
+        }
+    }
+    
+    var body: some View {
+        Text(TimerFunction(from: setDate))
+            .onAppear(perform: {self.timer
+            })
+    }
+    
+    func TimerFunction(from date: Date) -> String {
+        let calendar = Calendar(identifier: .japanese)
+        let timeValue = calendar
+            .dateComponents([.day, .hour, .minute, .second], from: nowDate, to: setDate)
+        return String(format: "残り"+"%02d日:%02d時間:%02d分:%02d秒",
+                      timeValue.day!,
+                      timeValue.hour!,
+                      timeValue.minute!,
+                      timeValue.second!)
+    }
+    
 }
